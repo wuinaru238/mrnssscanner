1d8b675516f99e7105e44ff23bb5ae1c3acb911f
exister20 <yskmuhasebe@gmail.com>
2020-11-14 01:12:22 +0300

    Create mrnscanner.cpp

---
+++ mrnscanner.cpp
@@ -0,0 +1,120 @@
+#include <iostream>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+using namespace std;
+
+char command1[200];
+char command2[200];
+char command3[200];
+
+string name;
+string surname;
+string city;
+
+char nm[100];
+char srnm[100];
+char cty[100];
+
+void echo(string m){
+
+     cout << m << endl;
+
+}
+
+
+int main()
+{
+
+     system("clear");          
+
+     cout << endl << endl;
+
+     while (true) {
+
+     system("touch namedata.txt");
+     system("touch surnamedata.txt");
+     system("touch finaldata.txt");
+
+     cout << " __________________________________________"  << endl;
+     cout << "(__________________________________________)" << endl;
+     cout << "|------------| Mernis Tarayıcı |-----------|" << endl;
+     cout << "| Dev : NSS22                              |" << endl;
+     cout << "| Version : 0.0.1                          |" << endl;
+     cout << "| Mail : l24archl24@protonmail.ch          |" << endl;
+     cout << "| Dev TEAM : ArchL29                       |" << endl;
+     cout << "|__________________________________________|" << endl;
+     cout << "(__________________________________________)" << endl;
+
+     cout << endl;
+
+     cout << "Mernis dosyası ile aynı klasörde olması gerekir." << endl;
+     cout << "Mernis dosyasının adı data_dump.sql şeklinde olmalıdır" << endl;
+
+     cout << endl;
+     cout << endl;
+
+     cout << "Kullanıcı adı : NSS22 " << endl;
+     cout << endl << endl;
+
+     cout << "| --- İsim" << endl
+          << "| --- > ";
+     getline(cin , name);
+
+     cout << endl;
+
+     cout << "| --- Soyisim" << endl
+          << "| --- > ";
+     getline(cin , surname);
+
+     cout << endl;
+
+     cout << "| --- Sehir" << endl
+          << "| --- > ";
+     getline(cin , city);
+
+     cout << endl;
+
+     cout << "------ | Tarama başladı | ------" << endl;
+     cout << endl;
+     cout << "İsim : " << name << endl;
+     cout << "Soyisim : " << surname << endl;
+     cout << "Şehir : " << city << endl;
+
+     cout << endl;
+
+     system("rm namedata.txt");
+     system("rm surnamedata.txt");
+     system("rm finaldata.txt");
+
+     strcpy(srnm, surname.c_str());
+     strcpy(nm, name.c_str());
+     strcpy(cty, city.c_str());
+
+     strcat(command1 , "grep \"");
+     strcat(command1 , nm);
+     strcat(command1 , "\"");
+     strcat(command1 , " data_dump.sql > namedata.txt");
+
+     system(command1);
+
+     strcat(command2 , "grep \"");
+     strcat(command2 , srnm);
+     strcat(command2 , "\" ");
+     strcat(command2 , "namedata.txt > surnamedata.txt");
+
+     system(command2);
+
+     strcat(command3 , "grep \"");
+     strcat(command3 , cty);
+     strcat(command3 , "\" ");
+     strcat(command3 , "surnamedata.txt > finaldata.txt");
+
+     system(command3);
+
+     system("nano finaldata.txt");
+
+     }
+
+}
